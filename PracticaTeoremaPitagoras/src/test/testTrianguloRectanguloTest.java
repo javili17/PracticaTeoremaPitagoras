48c7d81ac46adc7d8c6fddc848672cdf30461354
javili17 <vickfuria18@gmail.com>
2025-05-20 17:24:36 +0200

    ingreso

--- PracticaTeoremaPitagoras/src/test/testTrianguloRectanguloTest.java
+++ PracticaTeoremaPitagoras/src/test/testTrianguloRectanguloTest.java
@@ -1,36 +1,64 @@
 package test;
 
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import clases.TrianguloRectangulo;
 
 class testTrianguloRectanguloTest {
 
-    @Test
-    void testTrianguloPorDefecto() {
-        TrianguloRectangulo t1 = new TrianguloRectangulo();
-
-        double areaEsperada = 0.5;
-        double perimetroEsperado = 1 + 1 + Math.sqrt(2);
-        double hipotenusaEsperada = Math.sqrt(2);
-
-        assertEquals(areaEsperada, t1.area());
-        assertEquals(perimetroEsperado, t1.perimetro());
-        assertEquals(hipotenusaEsperada, t1.getC());
-    }
-
-    @Test
-    void testTrianguloValido() {
-        TrianguloRectangulo t2 = new TrianguloRectangulo(3, 4);
-
-        double areaEsperada = 6.0;
-        double perimetroEsperado = 3 + 4 + 5;
-        double hipotenusaEsperada = 5.0;
-
-        assertEquals(areaEsperada, t2.area());
-        assertEquals(perimetroEsperado, t2.perimetro());
-        assertEquals(hipotenusaEsperada, t2.getC());
-    }
-
-  
-    }
+	@BeforeAll
+	static void setup() {
+		TrianguloRectangulo tri1 = new TrianguloRectangulo();
+	}
+	@Test
+	void testArea() {
+		TrianguloRectangulo tri1 = null;
+		double resultadoObtenido = tri1.area();
+		double resultadoEsperado = 0.5;
+		assertEquals(resultadoObtenido, resultadoEsperado);
+	}
+	@Test
+	void testHipotenusa() {
+		TrianguloRectangulo tri1 = null;
+		double resultadoObtenido = tri1.hipotenusa();
+		double resultadoEsperado = 1.41;
+		assertEquals(resultadoObtenido, resultadoEsperado, 0.01);
+	}
+	@Test
+	void testPerimetro() {
+		TrianguloRectangulo tri1 = null;
+		double resultadoObtenido = tri1.perimetro();
+		double resultadoEsperado = 3.41;
+		assertEquals(resultadoObtenido, resultadoEsperado, 0.01);
+	}
+	@Test
+	void testArea2() {
+		TrianguloRectangulo tri2 = new TrianguloRectangulo(2, 4);
+		double resultadoObtenido = tri2.area();
+		double resultadoEsperado = 4;
+		assertEquals(resultadoObtenido, resultadoEsperado);
+	}
+	@Test
+	void testHipotenusa2() {
+		TrianguloRectangulo tri2 = new TrianguloRectangulo(2, 4);
+		double resultadoObtenido = tri2.hipotenusa();
+		double resultadoEsperado = 4.47;
+		assertEquals(resultadoObtenido, resultadoEsperado, 0.01);
+	}
+	@Test
+	void testPerimetro2() {
+		TrianguloRectangulo tri2 = new TrianguloRectangulo(2, 4);
+		double resultadoObtenido = tri2.perimetro();
+		double resultadoEsperado = 10.47;
+		assertEquals(resultadoObtenido, resultadoEsperado, 0.01);
+	}
+	@Test
+	void testExcepcion() {
+		Exception E = assertThrows(IllegalArgumentException.class, () -> new TrianguloRectangulo(-1, -3));
+		String mensajeEsperado = "Los catetos NO pueden tener valores negativos";
+		String mensajeObtenido = E.getMessage();
+		assertEquals(mensajeObtenido, mensajeEsperado);
+	}
+}
